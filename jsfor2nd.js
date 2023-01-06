806f6a0b06ca7bdd02b6160fe51c4009628473c7
Rahulkakkireni <100458777+Rahulkakkireni@users.noreply.github.com>
2023-01-06 15:19:30 +0530

    Add files via upload

---
+++ jsfor2nd.js
@@ -0,0 +1,21 @@
+
+  function showb(){
+    if( document.getElementById('div1').style.display ='none'){
+      document.getElementById("div2").style.display='block';
+    }
+  }
+  function showa(){
+    if( document.getElementById('div1').style.display ='block'){
+      document.getElementById("div2").style.display='none';
+    }
+  }
+  function showba(){
+    if( document.getElementById('div4').style.display ='none'){
+        document.getElementById("div3").style.display='block';
+    }
+  }
+  function showatm(){
+    if( document.getElementById('div4').style.display ='block'){
+      document.getElementById("div3").style.display='none';
+    }
+  }
